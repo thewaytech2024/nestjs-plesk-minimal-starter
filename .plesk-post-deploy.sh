e665f9e7b2f18f3607a3c4485055503bb10065b5
peerasut <peerasutwong@hotmail.com>
2025-08-01 12:25:06 +0700

    add /test2 API again

--- .plesk-post-deploy.sh
+++ .plesk-post-deploy.sh
@@ -11,11 +11,13 @@ npm install
 
 # Build the application
 echo "🔨 Building application..."
-# npm run build
+npm run build
 
 # Run the application
 echo "🚀 Running application..."
 npm run start:prod
 
+# Restart the application
+touch tmp/restart.txt
 echo "✅ Post-deployment process completed!"
 echo "📝 Application should be available at: http://member-api-dev.thaibible.or.th" 
